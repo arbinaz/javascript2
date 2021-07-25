8deb4a4a6242539e01ff87bb5f6f8c084c4571e4
arbinaz <arbinaz05@gmail.com>
2021-07-25 10:55:43 +0530

    modified code

--- index.js
+++ index.js
@@ -2,4 +2,28 @@ function add(a,b){
     var sum=a+b;
     return sum;
 }
-var res=console.log(add(5,5))   ;
+var res=console.log(add(5,5));
+
+// function call without parameter
+function print(){
+    console.log("Hello World");
+}
+add()
+// function call with parameter
+function add(a,b)
+{
+    console.log(a+b);
+}
+add(5,5)
+// Function Expression
+var sum1=function(c,d){
+    var res1=(c+d)
+    return res1;
+}
+sum1(12,5)
+// function hoisting
+console.log(sample(15,7))
+function sample(e,f){
+    var res2=e+f;
+    return res2
+}
